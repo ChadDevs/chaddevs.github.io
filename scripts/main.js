1928a5b339ba807fa05ba1f26817f6ce57a8063e
ChadDevs <106409369+ChadDevs@users.noreply.github.com>
2022-05-27 17:58:52 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/Monke.jpg') {
+    myImage.setAttribute ('src','images/sussy-among-us.gif');
+  } else {
+    myImage.setAttribute ('src','images/Monke.jpg');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Thank you for visiting: ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Thank you for visiting: ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
